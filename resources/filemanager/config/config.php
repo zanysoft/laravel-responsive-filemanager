922e3dd7d65ade0046c6218358befda5e8b3cfda
Mubashar Ahmad <mubashar.ahmad@qrgsolutions.com>
2022-01-28 19:04:39 +0500

    no message

--- resources/filemanager/config/config.php
+++ resources/filemanager/config/config.php
@@ -84,7 +84,7 @@ $config = array(
     | without final / (DON'T TOUCH)
     |
     */
-    'base_url' => ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http") . "://" . @$_SERVER['HTTP_HOST'],
+    'base_url' => env('APP_URL', (((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http") . "://" . @$_SERVER['HTTP_HOST'])),
     /*
     |--------------------------------------------------------------------------
     | path from base_url to base of upload folder
